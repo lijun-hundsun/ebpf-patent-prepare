f0c66d8e47c1a7b7def3f88664b4eaac06756731
lijun-hundsun <117134590+lijun-hundsun@users.noreply.github.com>
2023-11-16 09:00:15 +0000

    just a test again

--- cmd/ringbuffer/main.go
+++ cmd/ringbuffer/main.go
@@ -86,6 +86,6 @@ func main() {
 			continue
 		}
 
-		log.Printf("lijun debug pid: %d\tcomm: %s\n", event.Pid, unix.ByteSliceToString(event.Comm[:]))
+		log.Printf("lijun debug1 pid: %d\tcomm: %s\n", event.Pid, unix.ByteSliceToString(event.Comm[:]))
 	}
 }
